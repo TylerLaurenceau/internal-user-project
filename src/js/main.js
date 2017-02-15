84a2e6921690c79ace2a9451c49de4ef3f9ff95b
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-15 17:14:29 -0500

    almost finished styling

--- src/js/main.js
+++ src/js/main.js
@@ -12,13 +12,14 @@ function getInfo(data) {
   console.log (data)
   for (var i=0;i<data.results.length;i++){
     $(".container").append(`
-    <div class = "personContainer">
-    <ul class = "person">
-    <li class = "picture"><img src = "${data.results[i].picture.large}"/></li>
-    <li class = "name">${data.results[i].name.first} ${data.results[i].name.last}</li>
-    <li class = "email">${data.results[i].email}</li>
-    <li class = "adress">${data.results[i].location.street} <br/> ${data.results[i].location.city}. ${data.results[i].location.state}. ${data.results[i].location.postcode}</li>
-    <li class = "phone">${data.results[i].phone}</li>
+    <div>
+    <ul>
+    <div class = "personContainer"><li class = "picture"><img src = "${data.results[i].picture.large}"/></li></div>
+    <div class = "personContainer"><li class = "name"><b>${data.results[i].name.first} ${data.results[i].name.last}</b></li></div>
+    <div class = "personContainer"><li class = "email">${data.results[i].email}</li></div>
+    <div class = "personContainer"><li class = "adress">${data.results[i].location.street} <br/>${data.results[i].location.city}. ${data.results[i].location.state}. ${data.results[i].location.postcode}</li></div>
+    <div class = "personContainer"><li class = "phone">${data.results[i].phone}</li></div>
+    </ul>
     </div>`)
   }
 }
