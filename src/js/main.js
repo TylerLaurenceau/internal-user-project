6e431292c165ba59ded5f41790489b83a3823fd1
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-15 17:38:11 -0500

    finished styling

--- src/js/main.js
+++ src/js/main.js
@@ -15,8 +15,8 @@ function getInfo(data) {
     <div>
     <ul>
     <div class = "personContainer"><li class = "picture"><img src = "${data.results[i].picture.large}"/></li></div>
-    <div class = "personContainer"><li class = "name"><b>${data.results[i].name.first} ${data.results[i].name.last}</b></li></div>
-    <div class = "personContainer"><li class = "email">${data.results[i].email}</li></div>
+    <div class = "personContainer"><li class = "name"><span><b>${data.results[i].name.first.toUpperCase()} ${data.results[i].name.last.toUpperCase()}</b></span></li></div>
+    <div class = "personContainer"><li class = "email">${data.results[i].email.toUpperCase()}</li></div>
     <div class = "personContainer"><li class = "adress">${data.results[i].location.street} <br/>${data.results[i].location.city}. ${data.results[i].location.state}. ${data.results[i].location.postcode}</li></div>
     <div class = "personContainer"><li class = "phone">${data.results[i].phone}</li></div>
     </ul>
