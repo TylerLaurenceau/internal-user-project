6898dd1fea16d15e2478e7200091d2de0af42a40
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-02-16 09:15:48 -0500

    updated link to live site

--- src/js/main.js
+++ src/js/main.js
@@ -14,12 +14,12 @@ function getInfo(data) {
     $(".container").append(`
     <div>
     <ul>
-    <div class = "personContainer"><li class = "picture"><img src = "${data.results[i].picture.large}"/></li></div>
-    <div class = "personContainer"><li class = "name"><span><b>${data.results[i].name.first.toUpperCase()} ${data.results[i].name.last.toUpperCase()}</b></span></li></div>
-    <div class = "personContainer"><li class = "email">${data.results[i].email.toUpperCase()}</li></div>
-    <div class = "personContainer"><li class = "adress">${data.results[i].location.street} <br/>${data.results[i].location.city}. ${data.results[i].location.state}. ${data.results[i].location.postcode}</li></div>
-    <div class = "personContainer"><li class = "phone">${data.results[i].phone}</li></div>
+    <div class = "personContainer"><li class = "picture"><img src = "${data.results[i].picture.large}"/></li>
+    <li class = "name"><span><b>${data.results[i].name.first.toUpperCase()} ${data.results[i].name.last.toUpperCase()}</b></span></li>
+    <li class = "email">${data.results[i].email.toUpperCase()}</li>
+    <li class = "adress">${data.results[i].location.street} <br/>${data.results[i].location.city}. ${data.results[i].location.state}. ${data.results[i].location.postcode}</li>
+    <li class = "phone">${data.results[i].phone}</li></div>
     </ul>
-    </div>`)
+    `)
   }
 }
